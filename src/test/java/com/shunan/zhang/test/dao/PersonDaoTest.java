4ff140d25a4dbefcbe02d0a015eef28b2057952b
unknown <1404876006@qq.com>
2018-08-14 15:40:55 +0800

    test

---
+++ src/test/java/com/shunan/zhang/test/dao/PersonDaoTest.java
@@ -0,0 +1,75 @@
+package com.shunan.zhang.test.dao;
+
+import com.shunan.zhang.test.BaseTest;
+import com.shunanzhang.dao.PersonDao;
+import com.shunanzhang.entity.PersonInfo;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+public class PersonDaoTest extends BaseTest {
+    @Autowired
+    private PersonDao personDao;
+
+    @Ignore
+    @Test
+    public void insertPersonInfo() throws Exception {
+        PersonInfo personInfo = new PersonInfo();
+        personInfo.setPersonName("zhang");
+        personInfo.setPassWord("111111");
+        personInfo.setGender(1);
+        personInfo.setPhone("18801183907");
+        personInfo.seteMail("1404876006@qq.com");
+        personInfo.setCreateTime(new Date());
+        personInfo.setLastEditTime(new Date());
+        personDao.insertPersonInfo(personInfo);
+    }
+    @Ignore
+    @Test
+    public void queryPersonInfoById()  {
+        int x=0;
+        try {
+            personDao.queryPersonInfoById(12L);
+        }catch(Exception e){
+            System.out.println(e instanceof RuntimeException);
+        }
+    }
+   @Ignore
+    @Test
+    public void updatePersonInfo() throws Exception {
+        PersonInfo perinfo = new PersonInfo();
+        perinfo.setPersonId(12L);
+        perinfo.setPassWord("12011232");
+        int effectnum=personDao.updatePersonInfo(perinfo);
+        System.out.print(effectnum);
+    }
+
+    @Test
+    public void deletePersonInfo() throws Exception {
+        personDao.deletePersonInfo(13L);
+    }
+    @Ignore
+    @Test
+    public void getPersonInfoTest(){
+        PersonInfo perinfo = new PersonInfo();
+        perinfo.setGender(1);
+        perinfo.setPersonName("zh");
+        SimpleDateFormat bartDateFormat = new SimpleDateFormat("MM-dd-yyyy");
+        Date dt=null;
+        String dateStringToParse = "7-24-2018";
+        try{
+            dt=bartDateFormat.parse(dateStringToParse);
+        }catch(Exception e){
+
+        }
+        System.out.println("date"+dt);
+        perinfo.setCreateTime(dt);
+        List<PersonInfo> results=personDao.getPersonInfoList( perinfo,0,2);
+        List<PersonInfo> results2=personDao.getPersonInfoList( perinfo,2,2);
+
+    }
+}
