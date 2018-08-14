4fc56b7a9f877c107115821d610201c174a68e6e
unknown <1404876006@qq.com>
2018-08-14 09:50:56 +0800

    test

--- src/main/Java/com/shunanzhang/web/TaskController.java
+++ src/main/Java/com/shunanzhang/web/TaskController.java
@@ -1,4 +1,24 @@
 package com.shunanzhang.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@RequestMapping(value="task",method = {RequestMethod.GET,RequestMethod.POST})
 public class TaskController {
+    @RequestMapping(value = "/test",method=RequestMethod.GET)
+    @ResponseBody
+    private Map ownerLoginCheck(HttpServletRequest request) {
+
+          Map<String,String> result=new HashMap<String,String>();
+          result.put("rest","success");
+          return result;
+    }
 }
