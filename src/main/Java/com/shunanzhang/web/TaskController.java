e0fffd971fd34117075ce8cb277a3cb819a3d9c7
unknown <1404876006@qq.com>
2018-08-15 16:53:05 +0800

    test

--- src/main/Java/com/shunanzhang/web/TaskController.java
+++ src/main/Java/com/shunanzhang/web/TaskController.java
@@ -1,10 +1,15 @@
 package com.shunanzhang.web;
 
+import com.shunanzhang.entity.PersonInfo;
+import com.shunanzhang.service.IPersonService;
+import org.apache.ibatis.annotations.Param;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -15,7 +20,12 @@ import java.util.Map;
 @Controller
 @RequestMapping(value="task",method = {RequestMethod.GET,RequestMethod.POST})
 public class TaskController {
+
     private static final Logger logger = LoggerFactory.getLogger(TaskController.class);
+
+    @Autowired
+    private IPersonService iPersonService;
+
     @RequestMapping(value = "/test",method=RequestMethod.GET)
     @ResponseBody
     private Map ownerLoginCheck(HttpServletRequest request) {
@@ -26,5 +36,18 @@ public class TaskController {
         logger.error("test");
           result.put("rest","success");
           return result;
+    }
+    @RequestMapping(value = "/find",method=RequestMethod.GET)
+    @ResponseBody
+    private Map queryCustomeerInfo(@RequestParam("personid") long personId){
+        Map<String ,Object> results=new HashMap<String,Object>();
+        PersonInfo  personInfo=iPersonService.getCustomerInfo(personId);
+        if(personInfo==null){
+            logger.info("query customer info failed ,customer :{}",personInfo);
+            results.put("success",false);
+            return results;
+        }
+        results.put("success",true);
+        return results;
     }
 }
